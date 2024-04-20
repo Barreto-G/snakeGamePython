83b856109941e15392f031d40f890d52fbd6de6a
G. Barreto <81831041+Barreto-G@users.noreply.github.com>
2024-04-20 16:39:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# snakeGamePython
+ Minha implementação do famoso jogo da cobrinha utilziando a biblioteca pygame
---
+++ main.py
@@ -0,0 +1,12 @@
+
+# 1 etapa - Definir configuracoes iniciais
+
+# 2 etapa - Loop infinito
+#   2.1 - Analisar entradas do usuario
+#   2.2 - atualizar elementos na tela
+#   2.3 - Criar logica de fim de jogo
+
+
+if __name__ == '__main__':
+    print('PyCharm')
+
